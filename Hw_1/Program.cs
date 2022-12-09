27f6cba900578b4c5fba7543f47ffa9e090095aa
Sofia <sayana-tv@ya.ru>
2022-12-09 18:19:53 +0800

    Задача 4

--- Hw_1/Program.cs
+++ Hw_1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
 
-
+/*
 Console.Write("Input a number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -10,3 +10,23 @@ if(num1 >= num2)
     Console.WriteLine("Max = " + num1);
 else
     Console.WriteLine("Max = " + num2);
+*/
+
+// Задача 4. Напишите программу, которая принимает на вход три числа  и выдает максимальное из этих чисел.
+
+
+Console.Write("Input a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if(num2 > max)
+    max = num2;
+    if(num3 > max)
+        max = num3;
+
+Console.WriteLine("Наибольшее число " + max);
+
